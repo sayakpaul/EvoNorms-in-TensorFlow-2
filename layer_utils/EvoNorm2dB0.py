4e503b7f35499f5ff02682720a840a818fb3a236
Sayak Paul <spsayakpaul@gmail.com>
2020-04-20 07:18:25 +0530

    Update EvoNorm2dB0.py
    As per https://github.com/sayakpaul/EvoNorms-in-TensorFlow-2/issues/1#issuecomment-616189155

--- layer_utils/EvoNorm2dB0.py
+++ layer_utils/EvoNorm2dB0.py
@@ -3,14 +3,14 @@
 
 import tensorflow as tf
 
-def instance_std(x, eps=1e-5):
+def instance_std(x, eps=1e-3):
 	# https://www.tensorflow.org/api_docs/python/tf/nn/moments
 	_, var = tf.nn.moments(x, axes=[1, 2], keepdims=True)
 	return tf.sqrt(var + eps)
 
 class EvoNorm2dB0(tf.keras.layers.Layer):
-	def __init__(self, in_channels, nonlinear=True, momentum=0.9,
-		eps=1e-5):
+	def __init__(self, in_channels, nonlinear=True, momentum=0.99,
+		eps=1e-3):
 		super(EvoNorm2dB0, self).__init__()
 		self.nonlinear = nonlinear
 		self.momentum = momentum
@@ -33,16 +33,13 @@ class EvoNorm2dB0(tf.keras.layers.Layer):
 			N, H, W, C = tf.shape(x)
 
 			if self.training:
-				x1 = tf.transpose(x, [3, 0, 1, 2])
-				x1 = tf.reshape(x1, (C, -1))
-				var = tf.math.square(tf.math.reduce_std(x1, axis=1))
-				var = tf.reshape(var, (1, 1, 1, C))
+				_, var = tf.nn.moments(x, [0, 1, 2], keepdims=True)
 				self.running_var = self.momentum * self.running_var + (1 - self.momentum) * var
 			else:
 				var = self.running_var
 
 			if self.nonlinear:
-				den = tf.math.maximum(tf.sqrt(var), self.v * x + instance_std(x))
+				den = tf.math.maximum(tf.sqrt(var + self.eps), self.v * x + instance_std(x))
 				return x / den * self.gamma + self.beta
 			else:
 				return x * self.gamma + self.beta
